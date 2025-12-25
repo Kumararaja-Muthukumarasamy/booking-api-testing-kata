8d9f325a8054779a6fd2b0e27bf692fa52bd3724
Kumar <reach.mdkumar@gmail.com>
2025-12-25 12:56:32 +0530

    chore: add jackson serializer for auth request model

--- src/test/java/com/booking/client/AuthClient.java
+++ src/test/java/com/booking/client/AuthClient.java
@@ -3,18 +3,20 @@ package com.booking.client;
 import static io.restassured.RestAssured.*;
 
 import com.booking.constants.APIPaths;
+import com.booking.model.AuthRequest;
 import com.booking.spec.RequestSpecFactory;
 import io.restassured.response.Response;
 
 public class AuthClient {
-    private AuthClient(){
+    private AuthClient() {
 
     }
-    public static Response generateToken(String username, String password){
+
+    public static Response generateToken(String username, String password) {
+        AuthRequest request = new AuthRequest(username, password);
         return given()
                 .spec(RequestSpecFactory.getBaseRequestSpec())
-                .body("{\"username\":\""+ username + "\","+
-                       "\"password\": \"" + password + "\" }")
+                .body(request)
                 .when()
                 .post(APIPaths.AUTH_LOGIN);
     }
